302df86d28f98046f5f7f40a81c4d7289beda310
blablupcom <o.volodin@yahoo.com>
2018-07-25 20:37:32 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -93,8 +93,35 @@ data = []
 
 #### READ HTML 1.0
 
-html = requests.get(url, verify=False)
-soup = BeautifulSoup(html.text, 'lxml')
+import ssl
+import sys
+
+import requests
+
+from requests.utils import urlparse
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
+
+class Tls12HttpAdapter(HTTPAdapter):
+    """Transport adapter that forces use of TLSv1.2."""
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        """Create and initialize the urllib3 PoolManager."""
+        self.poolmanager = PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_version=ssl.PROTOCOL_TLSv1_2)
+
+
+url = sys.argv[1]
+
+s = requests.Session()
+s.mount('https://', Tls12HttpAdapter())
+r = s.get(url)
+print(r.status_code)
+
+
+# html = requests.get(url, verify=False)
+# soup = BeautifulSoup(html.text, 'lxml')
 
 #### SCRAPE DATA
 
